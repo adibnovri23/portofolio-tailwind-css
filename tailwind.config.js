858cff12c24e0287f3f2f2838734e4864672b38b
unknown <adibnovri23@gmail.com>
2024-04-02 23:38:46 +0700

    portofolio tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '130px',
+    },
+    extend: {
+      colors: {
+        stone: '#57534e',
+        secondary: '#64748b',
+        dark: '#0f172a',
+      },
+      screens: {
+        '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [],
+};
